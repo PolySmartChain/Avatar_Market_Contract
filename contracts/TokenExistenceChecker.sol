6e6e33ebefba63528afb65d7ad8c124df7026d44
Jim <jimcarrey032@proton.me>
2024-07-02 17:11:51 +0800

    update

--- contracts/TokenExistenceChecker.sol
+++ contracts/TokenExistenceChecker.sol
@@ -4,21 +4,29 @@ pragma solidity ^0.8.0;
 
 interface IERC721 {
     function getDate(uint256 tokenId) external view returns (uint256);
+}
 
+interface Proxys {
+    function hasMint(uint256[] calldata tokenIds) external view returns (bool);
 }
 
 contract TokenExistenceChecker {
     IERC721 private tokenContract;
+    Proxys private proxysContract;
 
-    constructor(address _tokenAddress) {
+    constructor(address _tokenAddress, address _proxys) {
         tokenContract = IERC721(_tokenAddress);
+        proxysContract = Proxys(_proxys);
     }
 
     function getNoExistentTokenId(uint256 from, uint256 to) public view returns (uint256) {
         uint256 emptyId = 0;
         for (uint256 i = from; i <= to; i++){
             uint256 date = tokenContract.getDate(i);
-            if(date == 0){
+            uint256[] memory array = new uint256[](1);
+            array[0] = i;
+            bool hasMint = proxysContract.hasMint(array);
+            if(date == 0 && hasMint == true){
                 emptyId = i;
                 break ;
             }
